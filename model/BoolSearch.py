dffb83b8a09cb4b0229d202b541de32bc4658faa
ipfgao <ipfgao@gmail.com>
2020-03-05 03:57:53 +0800

    优化布尔搜索，挺高准确率
    1、保留stop word，因为现在的stop word并不是很好，会裁掉很多在对话有意义的词。对于现有的对话库，保留stop word 效果较佳。
    2、以 提出的问题 跟 库中问题 所共有的词汇 在二者中的占比之积 做为二者的匹配度。避免单一匹配带来的歧义。

--- model/BoolSearch.py
+++ model/BoolSearch.py
@@ -4,12 +4,18 @@ import collections
 class BoolSearch:
     def __init__(self, qa_list):
         self.qa_list = qa_list
-        self.index_table = {}
+        self.index_table = {}#{"word a":[ida1,ida2,ida3..."word b":[idb1,idb2,idb3...]}
         for idx, qa in enumerate(self.qa_list):
-            for word in list(set(qa.question_words_nostop)):
+            #qa_cutted=list(set(qa.question_words_nostop))
+            qa_cutted=list(set(qa.question_words))# ipfgao更新：对于现有库，保留stop word 效果较佳，或者重新优化stop word列表
+
+            #避免在去掉stop word后出现空值
+            if len(qa_cutted)==0:qa_cutted=list(set(qa.question_words))
+                
+            for word in qa_cutted:
                 if word not in self.index_table.keys():
                     self.index_table[word] = []
-                self.index_table[word].append(idx)
+                self.index_table[word].append((idx,len(qa_cutted)))## ipfgao更新：记录qa的长度，方便提出的问题跟库里的问题进行对比
 
     def search(self, qa, top_k=10, scale=1.0):
         '''
@@ -18,18 +24,30 @@ class BoolSearch:
         :param top_k: 返回前top_k个最相似qa
         :param scale: 匹配度系数
         :return: 前top_k个最相似qa，以及qa的问题中包含查询句子单词的数量占比
-                [[qa, count/total_words_cnt], [qa, count/total_words_cnt], ...]
+                [[qa, count**2/(total_words_cnt*len(qa))], [qa, count**2/(total_words_cnt*len(qa))], ...]
         '''
-        query_counts = collections.Counter()
+        query_counts = collections.Counter()#计数器
         # 查找每个单词在哪些问题中出现过
-        total_words_cnt = len(qa.question_words_nostop)
-        for w in qa.question_words_nostop:
+        #qa_cutted=qa.question_words_nostop
+        qa_cutted=qa.question_words# ipfgao更新：对于现有库，保留stop word 效果较佳，或者重新优化stop word列表
+        total_words_cnt = len(qa_cutted)#总单词数
+        
+        #避免在去掉stop word后出现空值
+        if total_words_cnt==0:
+            qa_cutted=qa.question_words
+            total_words_cnt=len(qa_cutted)
+            
+        print(f"分词后的问题：{qa_cutted}")    
+        
+        for w in qa_cutted:
             query_res = self.index_table.get(w, [])
             query_counts.update(query_res)
-        # 找出现最高的topk个问题id
-        query_result = query_counts.most_common(top_k)
-        qa_result = [[self.qa_list[qid], (cnt * scale)/total_words_cnt] for qid, cnt in query_result]
 
+        #ipfgao更新：同频词太多，直接返回topk并不合理，应该返回最大的那个（或者那一群）,然后选取top_k个
+        max_cnt=max(query_counts.values())
+        # ipfgao更新：以 提出问题 跟 库中问题 共有词汇 在二者中的占比之积 做为二者的匹配度
+        qa_result = [[self.qa_list[qid[0]], (max_cnt * scale)**2/(total_words_cnt*qid[1])] for qid,cnt in query_counts.items() if cnt==max_cnt]
+        qa_result=sorted(qa_result, key=lambda x:x[1],reverse=True)[0:top_k]
         return qa_result
 
 if __name__ == '__main__':
